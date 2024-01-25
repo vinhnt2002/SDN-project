5681fcbbe5eeb9c9c3bc43bad66435139f4f0af5
Trần Nhật Tiến <trannhattien160802@gmail.com>
2024-01-25 13:05:03 +0700

    api

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -17,3 +17,68 @@ export const testGetAllUser = CatchAsyncErrors(
     }
   }
 );
+
+export const addUser = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const{ name, email, password} = req.body;
+        
+      await userModal.create({
+        name,
+        email,
+        password,
+      });
+      return res.status(200).json({
+        success: true,
+        message: "create successful",
+      
+      });
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+export const updateUser = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const{id} = req.params;
+
+      const{ name, email, password} = req.body;
+        
+
+      await userModal.findByIdAndUpdate(id,{
+        name,
+        email,
+        password,
+      });
+      const users = await userModal.findById(id);
+      return res.status(200).json({
+        success: true,
+        message: "update successful",
+        users,
+      });
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+
+export const deleteUser = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const{id} = req.params;
+
+      await userModal.findByIdAndDelete(id);
+      const users = await userModal.findById(id);
+      return res.status(200).json({
+        success: true,
+        message: "delete successful",
+        users,
+      });
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,13 +1,17 @@
 
 
 import express from 'express'
-import { testGetAllUser } from '../controllers/user.controller'
+import { addUser, deleteUser, testGetAllUser, updateUser } from '../controllers/user.controller'
 
 
 const userRouter = express.Router()
 
 userRouter.get("/users", testGetAllUser)
 
+userRouter.post("/adduser", addUser)
 
+userRouter.put("/updateUserbyId/:id", updateUser)
+
+userRouter.delete("/deleteUserbyId/:id", deleteUser)
 
 export default userRouter
