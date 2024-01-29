a72aede53a8f35bf6e9fac23f764b0f1d03b6aa4
Your Name <you@example.com>
2024-01-29 09:49:24 +0700

    as1

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -17,3 +17,91 @@ export const testGetAllUser = CatchAsyncErrors(
     }
   }
 );
+
+export const getAllUsers = CatchAsyncErrors(
+    async (req: Request, res:Response, next: NextFunction) => {
+      try {
+        
+        const users = await userModal.find({});
+        return res.status(200).json({
+          success: true,
+          users,
+        })
+
+      } catch (error : any) {
+        return next( new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+
+  export const addUser = CatchAsyncErrors(
+    async (req: Request, res:Response, next: NextFunction) => {
+      try {
+        
+        const {name, email, password} = req.body;
+
+        await userModal.create({
+          name,
+          email,
+          password
+          })
+
+        return res.status(200).json({
+          success: true,
+          message: "add thanh cong"
+        })
+
+      } catch (error : any) {
+        return next( new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+
+
+  export const updateUserByID = CatchAsyncErrors(
+    async (req: Request, res:Response, next: NextFunction) => {
+      try {
+        
+        const {id} = req.params;
+
+        const {name, email} = req.body;
+
+        const user = await userModal.findByIdAndUpdate(id, {
+          name,
+          email,
+        }, {new: true});
+
+
+        return res.status(200).json({
+          success: true,
+          user
+        })
+
+      } catch (error : any) {
+        return next( new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+
+
+  export const deleteUser = CatchAsyncErrors(
+    async (req: Request, res:Response, next: NextFunction) => {
+      try {
+        
+        const {id} = req.params
+
+        await userModal.findByIdAndDelete(id)
+
+        return res.status(200).json({
+          success: true,
+          message: "delete thanh cong"
+        })
+
+      } catch (error : any) {
+        return next( new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+
+
+
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,13 +1,20 @@
 
 
 import express from 'express'
-import { testGetAllUser } from '../controllers/user.controller'
+import { addUser, deleteUser, getAllUsers, testGetAllUser, updateUserByID } from '../controllers/user.controller'
 
 
 const userRouter = express.Router()
 
 userRouter.get("/users", testGetAllUser)
 
+userRouter.get("/get-user/:id", getAllUsers)
+
+userRouter.post("/add-user", addUser)
+
+userRouter.put("/update-user/:id", updateUserByID)
+
+userRouter.delete("/delete-user/:id", deleteUser)
 
 
 export default userRouter
